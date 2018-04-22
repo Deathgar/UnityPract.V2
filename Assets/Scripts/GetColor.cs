21b614d84fea86a435040ca4b9667bc7e94c1894
Vlad <Deathgar@users.noreply.github.com>
2018-04-22 11:17:23 +0300

    del

---
+++ Assets/Scripts/GetColor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GetColor : MonoBehaviour {
+    private List<RaycastResult> graphicsRaycasters;
+    private GraphicRaycaster gr;
+    private PointerEventData pointerEventData;
+    private List<string> tagsList;
+    // Use this for initialization
+    void Start () {
+        pointerEventData = new PointerEventData(GetComponent<EventSystem>());        
+        gr = gameObject.GetComponent<GraphicRaycaster>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+            graphicsRaycasters = new List<RaycastResult>();
+            tagsList = new List<string>();
+
+            pointerEventData.position = Input.mousePosition;
+            gr.Raycast(pointerEventData, graphicsRaycasters);
+
+            Debug.Log(graphicsRaycasters.Count);
+
+            foreach(RaycastResult r in graphicsRaycasters)
+            {
+                tagsList.Add(r.gameObject.tag);
+                Debug.Log(r.gameObject.tag);
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/VisibleButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisibleButtons : MonoBehaviour {
+    public Button first;
+    public Button second;
+    public Button third;
+
+    private void Start()
+    {
+        first.gameObject.active = false;
+        second.gameObject.active = false;
+        third.gameObject.active = false;
+    }
+
+    public void Change()
+    {
+        if(!first.gameObject.active)
+        {
+            Enable();
+        }
+        else
+        {
+            Disable();
+        }
+    }
+
+    private void Enable()
+    {
+        first.gameObject.active = true;
+        second.gameObject.active = true;
+        third.gameObject.active = true;
+    }
+
+    private void Disable()
+    {
+        first.gameObject.active = false;
+        second.gameObject.active = false;
+        third.gameObject.active = false;
+    }
+}
